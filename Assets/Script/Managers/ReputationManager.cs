9d0cdc328070980d5bd890a17d4272a9327c4dad
DarrenGameDev <166923450+DarrenGohGameDev@users.noreply.github.com>
2025-08-08 06:17:55 +0800

    Added reputation Manager
    Added reputation Manager

---
+++ Assets/Script/Managers/ReputationManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ReputationManager : MonoBehaviour
+{
+    public delegate void OnShopReputationChange(float rep);
+    public OnShopReputationChange onShopReputationChange;
+
+    private void OnEnable()
+    {
+        onShopReputationChange += SetShopReputation;
+    }
+
+    private void OnDisable()
+    {
+        onShopReputationChange -= SetShopReputation;
+    }
+
+    private void SetShopReputation(float rep)
+    {
+
+    }
+}
