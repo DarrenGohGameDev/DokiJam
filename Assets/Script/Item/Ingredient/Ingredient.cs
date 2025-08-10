3a7018546158697edf11bcc6572c22460ff7f3c5
DarrenGameDev <166923450+DarrenGohGameDev@users.noreply.github.com>
2025-08-10 22:45:35 +0800

    addded ingredient base class
    addded ingredient base class

---
+++ Assets/Script/Item/Ingredient/Ingredient.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Ingredient : MonoBehaviour
+{
+    [SerializeField] private BaseIngredientStatScriptableObject ingredientStat;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
