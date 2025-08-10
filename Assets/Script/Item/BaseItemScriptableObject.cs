f3e750fcc48c2d50cca006f60878c24cdb6afa02
DarrenGameDev <166923450+DarrenGohGameDev@users.noreply.github.com>
2025-08-10 18:24:16 +0800

    Added base item and base ingredient stat
    - Added base item and base ingredient stat

---
+++ Assets/Script/Item/BaseItemScriptableObject.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BaseItemStat", menuName = "ScriptableObjects/Item/BaseItemStat")]
+public class BaseItemScriptableObject : ScriptableObject
+{
+    public string itemName;
+    public string itemDescription;
+}
